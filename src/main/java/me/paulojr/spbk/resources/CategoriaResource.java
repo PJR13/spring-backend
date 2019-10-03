d4715ad66dd0886b105c4a41a80fdd4c89fb4852
Paulo Junior <eu@paulojr.me>
2019-10-03 03:28:45 -0300

    Testado GET Categorias

--- src/main/java/me/paulojr/spbk/resources/CategoriaResource.java
+++ src/main/java/me/paulojr/spbk/resources/CategoriaResource.java
@@ -1,15 +1,23 @@
 package me.paulojr.spbk.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import me.paulojr.spbk.model.Categoria;
+
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@GetMapping()
-	public String listar() {
-		return "REST funcional!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		List<Categoria> list = Arrays.asList(cat1, cat2);
+		return list;
 	}
 }
