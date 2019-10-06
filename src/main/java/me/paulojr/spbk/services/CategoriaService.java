b1bf3a7912cb1ed11fe2d7e29d664129be0abd91
Paulo Junior <eu@paulojr.me>
2019-10-06 05:18:14 -0300

    PUT e PATCH em Categoria

--- src/main/java/me/paulojr/spbk/services/CategoriaService.java
+++ src/main/java/me/paulojr/spbk/services/CategoriaService.java
@@ -26,6 +26,12 @@ public class CategoriaService {
 	}
 	
 	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		buscar(obj.getId());
 		return repo.save(obj);
 	}
 
