b2c2a182f5a4be7cc4c6ab357cf4a499a54d0f9b
Paulo Jr <eu@paulojr.me>
2019-12-28 15:48:43 -0300

    Validação de email repetido.

--- src/main/java/me/paulojr/spbk/repositories/ClienteRepository.java
+++ src/main/java/me/paulojr/spbk/repositories/ClienteRepository.java
@@ -1,11 +1,14 @@
 package me.paulojr.spbk.repositories;
 
+import me.paulojr.spbk.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import me.paulojr.spbk.domain.Cliente;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    @Transactional(readOnly = true)
+    Cliente findByEmail(String email);
 
 }
--- src/main/java/me/paulojr/spbk/services/validation/ClienteInsertValidator.java
+++ src/main/java/me/paulojr/spbk/services/validation/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package me.paulojr.spbk.services.validation;
 
+import me.paulojr.spbk.domain.Cliente;
 import me.paulojr.spbk.domain.enums.TipoCliente;
 import me.paulojr.spbk.dto.ClienteNewDTO;
+import me.paulojr.spbk.repositories.ClienteRepository;
 import me.paulojr.spbk.resources.exceptions.FieldMessage;
 import me.paulojr.spbk.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -11,17 +14,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+    @Autowired
+    private ClienteRepository repo;
+
     @Override
     public void initialize(ClienteInsert constraintAnnotation) { }
     @Override
     public boolean isValid(ClienteNewDTO value, ConstraintValidatorContext context) {
         List<FieldMessage> errors = new ArrayList<>();
         if (value.getTipo().equals(TipoCliente.PESSOAFISICA.getId()) && !BR.isValidCPF(value.getCpfOuCnpj())) {
-            errors.add(new FieldMessage("cpfOuCnpj", "CPF inválido."));
+            errors.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
         }
         if (value.getTipo().equals(TipoCliente.PESSOAJURIDICA.getId()) && !BR.isValidCNPJ(value.getCpfOuCnpj())) {
-            errors.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido."));
+            errors.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
         }
+        Cliente cli = repo.findByEmail(value.getEmail());
+        if(cli != null){
+            errors.add(new FieldMessage("email", "Email já existente"));
+        }
+
         errors.forEach(fieldMessage -> {
             context.disableDefaultConstraintViolation();
             context.buildConstraintViolationWithTemplate(fieldMessage.getMessage()).addPropertyNode(fieldMessage.getFieldName()).addConstraintViolation();
