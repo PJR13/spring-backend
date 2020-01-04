79edd207ac59a7bb948febb65090da786062b08f
PJR13 <eu@paulojr.me>
2020-01-04 06:53:23 -0300

    Profile Dev

---
+++ src/main/java/me/paulojr/spbk/config/DevConfig.java
@@ -0,0 +1,30 @@
+package me.paulojr.spbk.config;
+
+import me.paulojr.spbk.services.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+    @Autowired
+    private DbService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean initDatabase() throws ParseException {
+        if(!strategy.equalsIgnoreCase("create")){
+            return true;
+        }
+        dbService.initDB();
+        return true;
+    }
+
+}
