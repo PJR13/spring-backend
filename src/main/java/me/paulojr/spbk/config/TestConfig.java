5315fd8e87796fb89dd24aaac0b3591df713bb13
PJR13 <eu@paulojr.me>
2020-01-04 06:41:55 -0300

    Profile Teste

---
+++ src/main/java/me/paulojr/spbk/config/TestConfig.java
@@ -0,0 +1,23 @@
+package me.paulojr.spbk.config;
+
+import me.paulojr.spbk.services.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+    @Autowired
+    private DbService dbService;
+
+    @Bean
+    public boolean initDatabase() throws ParseException {
+        dbService.initDB();
+        return true;
+    }
+
+}
